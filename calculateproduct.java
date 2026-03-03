1c57937f2f10ae042caac91648a144342391a830
aditya0-ctrl <shevaleaditya@gmail.com>
2026-03-03 19:00:33 +0530

    Add files via upload

---
+++ calculateproduct.java
@@ -0,0 +1,13 @@
+public class calculateproduct {
+    public static int product(int a,int b){
+        int product=a*b;
+        
+        return product;
+    }
+    public static void main(String[] args) {
+        int a=3;
+        int b=2;
+        int multiply=product(a, b);
+        System.out.println(multiply);
+    }
+}
