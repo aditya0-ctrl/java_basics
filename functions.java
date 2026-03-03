3d1c649378cfd530f58e6269b420b0864e074201
aditya0-ctrl <shevaleaditya@gmail.com>
2026-03-03 19:01:34 +0530

    Add files via upload

---
+++ functions.java
@@ -0,0 +1,9 @@
+public class functions{
+    public static void printhelloworld(){
+        System.out.println("hello world");
+        return;
+    }
+    public static void main(String args[]) {
+        printhelloworld(); //this is function call
+    }
+}
