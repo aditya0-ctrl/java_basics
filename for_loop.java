9863202a21956ca9047b970b1fd536e565268ce2
aditya0-ctrl <shevaleaditya@gmail.com>
2026-03-09 20:18:22 +0530

    Add files via upload

---
+++ for_loop.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class for_loop{
+    public static void main(String args[]){
+        // for(int i=1;i<=10;i++){
+        //     System.out.println(i);
+        // }
+        for(int line=1;line<=4;line++){
+            System.out.println("****");
+        }
+    }
+}
